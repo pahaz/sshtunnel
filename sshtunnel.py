4f76afea34ac31e3ce7231346130fb064164bfce
J.M. Fernández <fernandez.cuesta@gmail.com>
2016-10-05 11:10:53 +0200

    Fix exception class (#75)
    * Increase version number
    * Enhance exception classes usability

--- sshtunnel.py
+++ sshtunnel.py
@@ -36,7 +36,7 @@ else:
     input_ = input
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'pahaz'
 
 
@@ -271,8 +271,8 @@ def _remove_none_values(dictionary):
 class BaseSSHTunnelForwarderError(Exception):
     """ Exception raised by :class:`SSHTunnelForwarder` errors """
 
-    def __init__(self, value, *args, **kwargs):
-        self.value = value
+    def __init__(self, *args, **kwargs):
+        self.value = kwargs.pop('value', args[0] if args else '')
 
     def __str__(self):
         return self.value
