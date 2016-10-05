47b046165cf7f6cb8c8602f2a00107c612d744a5
Jesús Fernández <fernandez.cuesta@gmail.com>
2016-10-05 10:42:10 +0200

    Enhance exception classes usability

--- sshtunnel.py
+++ sshtunnel.py
@@ -271,8 +271,8 @@ def _remove_none_values(dictionary):
 class BaseSSHTunnelForwarderError(Exception):
     """ Exception raised by :class:`SSHTunnelForwarder` errors """
 
-    def __init__(self, value, *args, **kwargs):
-        self.value = value
+    def __init__(self, *args, **kwargs):
+        self.value = kwargs.pop('value', args[0] if args else '')
 
     def __str__(self):
         return self.value
