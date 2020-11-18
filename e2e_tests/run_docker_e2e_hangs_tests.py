90d5b64ae4b68bffb7192a64db22d17c67ae7e20
Pahaz <pahaz.white@gmail.com>
2020-11-18 18:46:28 +0300

    CI: hungs tests

--- e2e_tests/run_docker_e2e_hangs_tests.py
+++ e2e_tests/run_docker_e2e_hangs_tests.py
@@ -1,8 +1,13 @@
 import logging
+import sshtunnel
+import os
+
 
 if __name__ == '__main__':
-    x = __import__('run_docker_e2e_db_tests')
+    path = os.path.join(os.path.dirname(__file__), 'run_docker_e2e_db_tests.py')
+    with open(path) as f:
+        exec(f.read())
     logging.warning('RUN')
-    tunnel = x.create_tunnel()
-    x.start(tunnel)
+    tunnel = create_tunnel()
+    start(tunnel)
     logging.warning('EOF')
