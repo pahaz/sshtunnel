21a8252f754ca4d9ea5445e5ecc67a7d8d6a5a51
Pahaz <pahaz.white@gmail.com>
2020-11-18 18:25:42 +0300

    CI: hungs tests

--- e2e_tests/run_docker_e2e_hangs_tests.py
+++ e2e_tests/run_docker_e2e_hangs_tests.py
@@ -1,8 +1,8 @@
 import logging
 
 if __name__ == '__main__':
-    from run_docker_e2e_db_tests import create_tunnel, start
+    x = __import__('run_docker_e2e_db_tests')
     logging.warning('RUN')
-    tunnel = create_tunnel()
-    start(tunnel)
+    tunnel = x.create_tunnel()
+    x.start(tunnel)
     logging.warning('EOF')
